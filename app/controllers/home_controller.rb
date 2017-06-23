31fc447771184b2499a7705b5fdcf332b3c0ebea
Hmhm lee <lhm1442@gmail.com>
2017-06-24 04:29:39 +0900

    for seminar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,9 @@
 class HomeController < ApplicationController
   def index
-    
+
   end
 
   def game
-    @x = params[:x].to_i
-    @x += 1
-    a = Random.new
-    @left =  a.rand(100)
-    @top = a.rand(100)
+
   end
 end
