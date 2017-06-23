3adef7bdc71e7549021c455eac5b2836b43a60bb
Hmhm lee <lhm1442@gmail.com>
2017-06-24 00:03:08 +0900

    merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   def index
+    
   end
 
   def game
+    @x = params[:x].to_i
+    @x += 1
+    a = Random.new
+    @left =  a.rand(100)
+    @top = a.rand(100)
   end
 end
